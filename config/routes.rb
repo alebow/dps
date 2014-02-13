895766c36e9434e501fa9c538fbb227d1e6db70c
Alex Lebow <alex.lebow@daversapartners.com>
2014-02-13 15:59:56 -0500

    Add a Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Dps::Application.routes.draw do
+  get "pages/home"
+  get "pages/about"
+  get "static_pages/home"
+  get "static_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

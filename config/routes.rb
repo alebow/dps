930455b689c468f5a1257c597b4d698b95d0da54
Alex Lebow <alex.lebow@daversapartners.com>
2014-02-15 14:18:27 -0500

    Finish user edit, update, index, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Dps::Application.routes.draw do
   match '/help',    to: 'pages#help',    via: 'get'
   match '/about',   to: 'pages#about',   via: 'get'
   match '/contact', to: 'pages#contact', via: 'get'
+  match '/locations',  to: 'pages#locations',  via: 'get'
   match '/signup',  to: 'users#new', 	 via: 'get'
   match '/signin',  to: 'sessions#new',  via: 'get'
   match '/signout', to: 'sessions#destroy', via: 'delete'
